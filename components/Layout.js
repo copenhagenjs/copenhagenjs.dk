d7cef2cad95db8ee15411246d92b49281f4c1c7f
Kevin Simper <kevin.simper@gmail.com>
2018-03-24 02:34:11 +0100

    Got it working in Next.js

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+export default ({children}) => {
+    return (
+      <div>
+        <Head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>CopenhagenJS</title>
+            <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900" type="text/css" />
+            <link rel="stylesheet" href="/static/stylesheets/style.css" />
+
+            <meta property="og:image" content="http://copenhagenjs.dk/static/images/logo.png"/>
+            <link href="/atom.xml" type="application/atom+xml" rel="alternate" title="CopenhagenJS ATOM Feed" />
+            <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        </Head>
+        <div>
+            {children}
+            <script async src="//platform.twitter.com/widgets.js"></script>
+            <script async src="//connect.facebook.net/en_US/all.js#xfbml=1&appId=423774561071001"></script>
+        </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,51 @@
-export default () => (<div>Hello World</div>)
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout>
+    <header className="page-header master bg-grey" role="navigation">
+        <ul className="navigation">
+            <li><a className="btn-white" href="/upcoming/">Upcoming events</a></li>
+            <li><a className="btn-white" href="/past/">Past events</a></li>
+            <li><a className="btn-white" href="/media/">Photos & Video</a></li>
+            <li><a className="btn-white" href="/about/">About</a></li>
+        </ul>
+
+        <img className="logo" src="/static/images/cphjs.png" alt="CopenhagenJS logo" />
+        <h3>CopenhagenJS is a monthly meetup for people interested in JavaScript in Copenhagen.</h3>
+
+        <a className="credits" href="http://www.flickr.com/photos/tenzer/8148224729/">Photo by Jeppe Toustrup</a>
+    </header>
+
+    <section className="page">
+      <div className="next-meetup">
+          <h1>Next meetup</h1>
+          <p>
+              Read more and sign up for the next event here:
+          </p>
+
+          <a className="next-meetup__button" href="https://www.meetup.com/copenhagenjs/">View meetup group</a>
+      </div>
+    </section>
+
+
+    <section className="newsletter">
+        <h3>Dont miss important news!</h3>
+
+        <div className="social">
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcopenhagenjs%2F&tabs&width=340&height=214&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=167461780321599" width="340" height="214" style={{"border":"none",overflow:"hidden"}} scrolling="no" frameBorder="0" allowtransparency="true"></iframe>
+            <a href="https://twitter.com/copenhagenjs" className="twitter-follow-button" data-show-count="true" data-size="large">Follow @copenhagenjs</a>
+        </div>
+
+        <div className="newsletter__description">
+            <p>Sign up for the CopenhagenJS newsletter and get notifications about upcoming events.</p>
+        </div>
+        <form action="//copenhagenjs.us11.list-manage.com/subscribe/post?u=e5f1b30e5ca3a2a397fa93a0d&amp;id=163c4118a6" method="post" target="_blank">
+            <input type="email" value="" placeholder="mail@example.com" name="EMAIL" className="newsletter__email" required />
+            <div style={{"position": 'absolute', left: "-5000px"}}>
+                <input type="text" name="b_e5f1b30e5ca3a2a397fa93a0d_163c4118a6" tabIndex="-1" value=""/>
+            </div>
+            <input type="submit" value="Subscribe now!" name="subscribe" className="newsletter__subscribe"/>
+        </form>
+    </section>
+  </Layout>
+)
