ef3e13fcb23314cdde59fad693e8c0720883f8de
Kevin Simper <kevin.simper@gmail.com>
2018-03-24 03:46:19 +0100

    Add page component

---
+++ components/Page.js
@@ -0,0 +1,37 @@
+import Layout from '../components/Layout'
+
+export default ({children}) => {
+  return (
+    <Layout>
+      <header className="page-header bg-grey" role="navigation">
+        <div className="page">
+          <a href="/">
+            <img className="logo" src="/static/images/cphjs.png" />
+          </a>
+
+          <ul className="navigation">
+            <li>
+              <a className="btn-white" href="/events/">
+                All events
+              </a>
+            </li>
+            <li>
+              <a className="btn-white" href="/media/">
+                Photos & Video
+              </a>
+            </li>
+            <li>
+              <a className="btn-white" href="/about/">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <section className="page">
+        {children}
+      </section>
+    </Layout>
+  )
+}
