4acd8e2235636bfef8d75a2d12a763d77bde5f57
Olga D <o.dmitricenko@gmail.com>
2018-04-11 12:07:50 +0200

    renamed pngs according #PR-55 corrections

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default ({ children }) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>CopenhagenJS</title>
-        <link rel="icon" type="image/png" href="/static/images/logo.png" />
+        <link rel="icon" type="image/png" href="/static/images/cphjs_small.png" />
         <link
           rel="stylesheet"
           href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900"
@@ -17,7 +17,7 @@ export default ({ children }) => {
 
         <meta
           property="og:image"
-          content="http://copenhagenjs.dk/static/images/logo-small.png"
+          content="http://copenhagenjs.dk/static/images/cphjs_large.png"
         />
         {/* TODO: use FeedBurner */}
         <link
