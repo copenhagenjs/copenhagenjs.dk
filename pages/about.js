9ea79b6e40d582ecd74e0a05dc5f92eadaf72b7a
Kevin Simper <kevin.simper@gmail.com>
2018-09-27 15:30:04 +0200

    Update about page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,14 @@
----
-title: About CopenhagenJS
-layout: single
----
+import marked from 'marked'
+import Page from '../components/Page'
 
-## About CopenhagenJS
+let text = `
+# About CopenhagenJS
 
 CopenhagenJS is a JavaScript user group in Copenhagen and the Öresund region. On the third thursday of every month we meet and share our knowledge, enthusiasm and war stories with each other.
 
 A wide range of backgrounds are represented in the group, from occasional users to JavaScript and front end experts. Everyone with an interest in JavaScript is welcome.
 
-In a typical meeting, we start at 19:00. There's a quick introduction, everyone grabs a drink of some sort. After that we usually have 3-4 talks of 15-30 minutes.
+In a typical meeting, we start at 18:00. There's a quick introduction, everyone grabs a drink of some sort. After that we usually have 3-4 talks of 15-30 minutes.
 
 Previous topics include:
 
@@ -22,7 +21,7 @@ Previous topics include:
 * Game programming with WebGL
 * Robots!
 
-Usually the meeting ends at around 22:00.
+Usually the meeting ends at around 21:00.
 
 If you're interested in attending a meeting, we'd appreciate an indication on the [Meetup page](https://www.meetup.com/copenhagenjs/), so we can get an idea of the requirements for the venue in terms of chairs, drinks, etc.
 
@@ -59,23 +58,17 @@ If you need help organising drinks, cables, put out chairs, etc, just let the or
 
 Signage helps people find your building, floor or office if you aren't located somewhere completely obvious. We encourage you to make use of our logo and put up signage in relevant places to give our attendees some breadcrumbs to follow.
 
-Download our logo here: [SVG](/images/logo/cphjs.svg), [PDF](/images/logo/cphjs.pdf)
+Download our logo here: [PNG](/static/images/cphjs.png)
 
 ### How do I contact you?
 
-Feel free to write to us at <hello@copenhagenjs.dk>, or on [@copenhagenjs](http://twitter.com/copenhagenjs), if you have questions, want to host, or simply wanna talk.
-
-We also have a [CopenhagenJS on Google Groups](http://groups.google.com/group/copenhagenjs/), and many of us hang out in our [#copenhagenjs channel on irc.freenode.net](irc://irc.freenode.net/copenhagenjs).
-
-<footer class="organizers">
-    <h4>Meet our organizers</h4>
-    <ul class="organizers">
-      <li>
-          <h5>Kevin Simper</h5>
-          <a href="https://kevinsimper.dk" title="Kevin Simper">
-            <img src="https://pbs.twimg.com/profile_images/421755629078728704/eTx6RTWJ.jpeg" alt="Kevin Simper">
-          </a>
-          <a href="https://twitter.com/kevinsimper" class="twitter-follow-button" data-show-count="true">Follow @kevinsimper</a>
-      </li>
-    </ul>
-</footer>
+Feel free to write to us at <kevin.simper@gmail.com>, or on [@copenhagenjs](http://twitter.com/copenhagenjs), if you have questions, want to host, or simply wanna talk.
+`
+export default () => {
+  return (
+    <Page>
+      <div dangerouslySetInnerHTML={{__html: marked(text)}}>
+      </div>
+    </Page>
+  )
+}
