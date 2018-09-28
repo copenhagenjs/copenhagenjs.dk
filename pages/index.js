b39934598408048b7732076d429763c5de7617b3
Kevin Simper <kevin.simper@gmail.com>
2018-09-28 04:05:24 +0200

    Update with latest meetup

--- pages/index.js
+++ pages/index.js
@@ -26,24 +26,24 @@ export default () => (
     </header>
 
     <section className="page">
+      <div style={{textAlign: 'center'}}><a style={{background: '#fae209', color: 'black', padding: '10px', border: '3px dashed black', borderRadius: '4px'}} href="/archive/2018-09-28-Blogging-challenge">Link to Blogging Challenge</a></div>
       <div
         dangerouslySetInnerHTML={{
           __html: marked(`
-# September Meetup - 27th
+# October Meetup 25th
 
-It's time for the September edition of CopenhagenJS. Let us get together and talk about JavaScript and what cool things we can do with it! We are looking for a host, can you help?
+Next meetup is going to be at Danmarks Radio.
 
 ### Schedule:
 
     17:00 - Doors open
     17:45 - Welcome
-    18:00 - "Video from Bornhack" - Julian
+    18:00 - You?
     18:25 - Break with food and drinks
-    19:00 - "How we use Glimmer.js" - Kenneth Larsen
-    19:35 - "Data Modelling with Types" - Andreas Møller
-    20:00 - "RailsGirls Summer of Code" - Neer & Anne
-    20:10 - Quiz
-    20:20 - Socialising - meet the community
+    19:00 - You?
+    19:35 - You?
+    20:00 - Quiz
+    20:10 - Socialising - meet the community
     21:00 - See you next time!
 
 ### What is CopenhagenJS?
@@ -60,7 +60,8 @@ Yes, you can write to us here on the meetup.com page if you want to help, or if 
 
 Looking forward to seeing you!
 
-Kevin & Paul`)
+Kevin & Paul
+          `)
         }}
       />
       <div className="next-meetup">
