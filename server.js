034b690ccef56a972c4edd5c35f1fa270a2cdbdc
Mathias Lund <jenslund@jenss-imac-2.home>
2018-11-25 21:58:48 +0100

    changed from var to let

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //var csrf_guid = Guid.raw();
-var csrf_guid = 'test';
+const csrf_guid = 'test';
 const account_kit_api_version = 'v1.1';
 const app_id = '248317325862187';
 const app_secret = 'b50a135544538e8160de917d6d346160';
@@ -20,24 +20,24 @@ const token_exchange_base_url = 'https://graph.accountkit.com/{{ACCOUNT_KIT_API_
 app.post('/login_success', cors(), function(request, response) {
   // CSRF check
   if (request.body.csrf === csrf_guid) {
-    var app_access_token = ['AA', app_id, app_secret].join('|');
-    var params = {
+    let app_access_token = ['AA', app_id, app_secret].join('|');
+    let params = {
       grant_type: 'authorization_code',
       code: request.body.code,
       access_token: app_access_token
     };
 
     // exchange tokens
-    var token_exchange_url = token_exchange_base_url + '?' + Querystring.stringify(params);
+    let token_exchange_url = token_exchange_base_url + '?' + Querystring.stringify(params);
     Request.get({url: token_exchange_url, json: true}, function(err, resp, respBody) {
-      var view = {
+      let view = {
         user_access_token: respBody.access_token,
         expires_at: respBody.expires_at,
         user_id: respBody.id,
       };
 
       // get account details at /me endpoint
-      var me_endpoint_url = me_endpoint_base_url + '?access_token=' + respBody.access_token;
+      let me_endpoint_url = me_endpoint_base_url + '?access_token=' + respBody.access_token;
       Request.get({url: me_endpoint_url, json:true }, function(err, resp, respBody) {
         // send login_success.html
         if (respBody.phone) {
