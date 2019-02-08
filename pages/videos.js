fe000dfe1c43e91dcdce944803692e67b54b6e77
Kevin Simper <kevin.simper@gmail.com>
2019-02-08 16:47:42 +0100

    Add the first three videos

--- pages/videos.js
+++ pages/videos.js
@@ -3,6 +3,41 @@ import Page from "../components/Page";
 
 export default class Videos extends React.Component {
   render() {
-    return <Page>Videos</Page>;
+    return (
+      <Page>
+        <h1>Videos</h1>
+        <p>
+          <a href="https://www.youtube.com/channel/UCOD8lwED5PAcgmhwymQJsng?view_as=subscriber">
+            Subscribe to us on Youtube
+          </a>
+        </p>
+        <p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/Zu8P6xejHuU"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/HYqxc_zjqMg"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VNoDZihhWHI"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        </p>
+      </Page>
+    );
   }
 }
