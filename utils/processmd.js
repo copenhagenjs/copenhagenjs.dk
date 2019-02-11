c27bc72551d47ef49d06980b37cd76d10fbdc9a7
Kevin Simper <kevin.simper@gmail.com>
2019-02-11 21:52:25 +0100

    Fix bug in process where it checked for a markdown operator

--- utils/processmd.js
+++ utils/processmd.js
@@ -1,26 +1,28 @@
-const { readdirSync, readFileSync, writeFileSync } = require('fs')
-const { promisify } = require('util')
-const marked = promisify(require('marked'))
+const { readdirSync, readFileSync, writeFileSync } = require("fs");
+const { promisify } = require("util");
+const marked = promisify(require("marked"));
 
-const files = readdirSync('./_posts/')
+const files = readdirSync("./_posts/");
 
 async function main() {
   for (let file in files) {
-    if (file === '_data.json') return
-    const content = readFileSync('./_posts/' + files[file], 'utf8')
-    const html = await marked(
-      content
-        .split('---')
-        .splice(2)
-        .join('')
-    )
+    console.log(files[file]);
+    if (files[file] === "_data.json") return;
+    const content = readFileSync("./_posts/" + files[file], "utf8");
+    const parsed = content.includes("---")
+      ? content
+          .split("---")
+          .splice(2)
+          .join("")
+      : content;
+    const html = await marked(parsed);
     writeFileSync(
-      './pages/archive/' + files[file].replace('.md', '.js'),
+      "./pages/archive/" + files[file].replace(".md", ".js"),
       `import Page from '../../components/Page'
         export default () => ( <Page><div dangerouslySetInnerHTML={{__html: \`${html}\`}}></div></Page> )
       `
-    )
+    );
   }
 }
 
-main()
+main();
