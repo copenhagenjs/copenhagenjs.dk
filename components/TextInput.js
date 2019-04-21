3200cfbcec7404536f0b53d2b2fc5f8a8b7051b4
Zoey Zou <zzo@trackman.com>
2019-04-21 14:13:14 +0200

    feat: add textinput component

---
+++ components/TextInput.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const TextInput = ({ type, label, name, onChange, width, height, padding, margin }) => (
+  <div className="input-root">
+    <label htmlFor={name}>{label}</label>
+    <input type={type} name={name} onChange={onChange} />
+    <style jsx>{`
+      .input-root {
+        display: flex;
+        flex-flow: column;
+        max-width: 300px;
+        margin: 10px auto;
+      }
+
+      label {
+        font-size: 12px;
+        text-transform: uppercase;
+        display: block;
+        font-family: 'Source Sans Pro', sans-serif;
+        color: #484848;
+        font-weight: 800;
+        etter-spacing: 0.08333333333333333em;
+      }
+
+      input {
+        box-sizing: border-box;
+        display: block;
+        max-width: 300px;
+        border: 1px solid #ebebeb;
+        border-radius: 4px;
+        outline-color: #f8de48;
+        font-size: 1rem;
+        width: ${width};
+        height: ${height ? height : '46.4px'};
+        margin: ${margin ? margin : '10px 0'};
+        padding: ${padding ? padding : '5px 4px 5px 12px'};
+      }
+    `}</style>
+  </div>
+);
+
+TextInput.defaultProps = {
+  type: 'text',
+};
+
+export default TextInput;
