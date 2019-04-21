e30e628ddc73931c8070e4943d560ce0d222b978
Zoey Zou <zzo@trackman.com>
2019-04-21 17:11:22 +0200

    feat: add contact page and formspree

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,20 @@
 import Page from '../components/Page';
+import TextInput from '../components/TextInput';
+import TextArea from '../components/TextArea';
+import Button from '../components/Button';
 
 export default () => {
   return (
     <Page>
-      <div>placeholder</div>
+      <form action="https://formspree.io/kevin.simper@gmail.com" method="POST">
+        <TextInput required label="Your name" name="name" />
+        <TextInput required type="email" label="Your email" name="email" />
+        <TextInput required label="Message title" name="title" />
+        <TextArea required label="Message body" name="message" />
+        <Button type="submit" size="lg" margin="20px auto">
+          Send
+        </Button>
+      </form>
     </Page>
   );
 };
