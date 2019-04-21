bc5a14876d309aae02c0a58bede051ec05c860f9
Zoey Zou <zzo@trackman.com>
2019-04-21 14:24:52 +0200

    feat: add textarea story

---
+++ stories/TextArea.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import TextArea from '../components/TextArea';
+
+storiesOf('Text Area', module).add('Default', () => (
+  <TextArea label="Default" name="default" onChange={action('changed')} />
+));
