2502a8c29be6a204f8a30c1158e5b517f9b1feab
Zoey Zou <zzo@trackman.com>
2019-04-21 13:33:33 +0200

    feat: add button story

---
+++ stories/Button.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from '../components/Button';
+
+storiesOf('Button', module)
+  .add('Small button', () => (
+    <Button size="sm" onClick={action('clicked')}>
+      Small button
+    </Button>
+  ))
+  .add('Large button', () => (
+    <Button size="lg" onClick={action('clicked')}>
+      Large button
+    </Button>
+  ));
