d00f050b9cbc38cb85ccd2f0783884482f35de49
Kevin Simper <kevin.simper@gmail.com>
2019-06-05 01:15:05 +0200

    Add small script to update list of posts

---
+++ utils/update-list.js
@@ -0,0 +1,7 @@
+const { readdirSync, writeFileSync } = require("fs");
+
+const dirs = readdirSync("./_posts").filter(a => a !== "_data.json");
+const data = {
+  posts: dirs
+};
+writeFileSync("./_posts/_data.json", JSON.stringify(data, null, 2) + "\n");
