bef238a09d58a881904c512d588ca5a826c19810
Kevin Simper <kevin.simper@gmail.com>
2019-06-12 04:16:18 +0200

    Add markdown headers automatically

--- _posts/2019-01-24-january-meetup.md
+++ _posts/2019-01-24-january-meetup.md
@@ -14,10 +14,12 @@ speakers:
     name: Peter Munter
 ---
 
+# January meetup at Siteimprove
+
 
 Hi friends, happy new year++ and we are ready to start another great year of CopenhagenJS. This time Siteimprove have been so friendly to host us. So join us to meet other JavaScript likeminded! 😄
 
-Schedule:
+## Schedule:
 17:00 - Doors open
 17:45 - Welcome
 18:00 - micro:bit - JavaScript meets microcontroller - Kenneth Geisshirt
@@ -29,10 +31,10 @@ Schedule:
 20:10 - Socialising - meet the community
 21:00 - See you next time!
 
-What is CopenhagenJS?
+## What is CopenhagenJS?
 it's a monthly community group in Copenhagen for all JavaScript developers. We get together and share the things we work on and really like. We talk about new frameworks and techniques that we can use in work to make it better.
 
-Can I present something at CopenhagenJS?
+## Can I present something at CopenhagenJS?
 Yes, CopenhagenJS is for and by the community so you are more than welcome to talk about things that you find interesting.
 
 Remember to Like our facebook to get all the latest updates and videos!
--- utils/fetch-meetup.js
+++ utils/fetch-meetup.js
@@ -1,10 +1,32 @@
-const argv = require('yargs').argv
-const fetch = require('node-fetch')
-const { writeFileSync } = require('fs')
+const argv = require("yargs").argv;
+const fetch = require("node-fetch");
+const { writeFileSync } = require("fs");
 
-async function main () {
-  const req = await fetch(`https://api.meetup.com/copenhagenjs/events/${argv.id}`)
-  const {description, link, name, local_date, local_time, venue} = await req.json()
+async function main() {
+  const req = await fetch(
+    `https://api.meetup.com/copenhagenjs/events/${argv.id}`
+  );
+  const {
+    description,
+    link,
+    name,
+    local_date,
+    local_time,
+    venue
+  } = await req.json();
+  const formatted = description
+    .replace(/<br\/>/g, "\n")
+    .replace(/<p>|<\/p>/g, "\n");
+  const headers = formatted
+    .split("\n")
+    .map(i => i.trim())
+    .map(i =>
+      i.includes("Schedule") || (i.length > 15 && i.includes("?")) // Length to fix schedule with questionsmark You?
+        ? "## " + i
+        : i
+    )
+    .join("\n");
+
   const output = `---
 title: ${name}
 type: meetup
@@ -16,11 +38,11 @@ duration: 3
 
 # ${name}
 
-${description.replace(/<br\/>/g, '\n').replace(/<p>|<\/p>/g, '\n')}`
-  console.log(output)
-  if(argv.file) {
-    writeFileSync(argv.file, output)
+${headers}`;
+  console.log(output);
+  if (argv.file) {
+    writeFileSync(argv.file, output);
   }
 }
 
-main()
+main();
