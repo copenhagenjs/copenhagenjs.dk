50a7a3aff976bdc4fdc26c3013ffaa67aade1bdc
Jonathan M. Hethey <public@jonathanmh.com>
2019-06-25 00:07:28 +0200

    removes unecessarily complex array handling

--- pages/speakers.js
+++ pages/speakers.js
@@ -3,17 +3,13 @@ import speakerEvents from '../static/speakers.json'
 
 export default () => {
 
-  const speakerList = [];
-
-  speakerEvents.map((event, id) => {
-    event.speakers.forEach(speaker => {
-      speakerList.push(
-        <li key={id}>
-          <a href={`/archive/${event.src.replace('.md', '')}`}>
-            {speaker.name}
-          </a>
-        </li>
-      )
+  const speakerList = speakerEvents.map((event, id) => {
+    return event.speakers.map(speaker => {
+      return <li key={`${id}-${speaker.name}`}>
+        <a href={`/archive/${event.src.replace('.md', '')}`}>
+          {speaker.name}
+        </a>
+      </li>
     });
   });
 
