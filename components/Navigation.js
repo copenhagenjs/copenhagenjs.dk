bc76ded6464e2514dc44b658b0babef9c5be8230
Kevin Simper <kevin.simper@gmail.com>
2019-08-08 00:53:40 +0200

    Better mobile menu when there is many items

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,36 +1,111 @@
 import React from 'react'
 
-export default () => (
-  <ul className="navigation">
-    <li>
-      <a className="btn-white" href="/">
-        Home
-      </a>
-    </li>
-    <li>
-      <a className="btn-white" href="/events">
-        All events
-      </a>
-    </li>
-    <li>
-      <a className="btn-white" href="/about">
-        About
-      </a>
-    </li>
-    <li>
-      <a className="btn-white" href="/videos">
-        Videos
-      </a>
-    </li>
-    <li>
-      <a className="btn-white" href="/contact">
-        Contact
-      </a>
-    </li>
-    <li>
-      <a className="btn-white" href="/speakers">
-        Speakers
-      </a>
-    </li>
-  </ul>
-)
+export default class Navigation extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showmenu: false
+    }
+  }
+  render() {
+    return (
+      <div>
+        <style jsx>{`
+          .btn-white {
+            text-decoration: none;
+            border: 1px solid #fff;
+            border-radius: 0;
+            padding: 10px 20px;
+            font-weight: bold;
+            color: #fff;
+            background-color: rgba(0, 0, 0, 0.5);
+            text-transform: uppercase;
+            transition: all 0.2s;
+          }
+          .btn-white:hover {
+            background: #f9e036;
+            color: #444;
+          }
+          .show-menu {
+            display: block;
+            margin: 10px 10px;
+          }
+          .navigation {
+            display: none;
+            margin: 0;
+            padding: 0;
+          }
+          @media (max-width: 800px) {
+            .navigation li a {
+              display: block;
+              line-height: 28px;
+              padding: 5px 10px;
+              margin: 10px 10px;
+            }
+          }
+          @media (min-width: 800px) {
+            .show-menu {
+              display: none;
+            }
+            .navigation {
+              display: flex;
+            }
+            .navigation li {
+              display: inline-block;
+              line-height: 28px;
+            }
+          }
+        `}</style>
+        <a
+          className="btn-white show-menu"
+          onClick={e => {
+            e.preventDefault()
+            this.setState({
+              showmenu: !this.state.showmenu
+            })
+          }}
+          href="#"
+        >
+          {this.state.showmenu ? 'Hide' : 'Show'} Menu
+        </a>
+        <ul
+          className="navigation"
+          style={{
+            display: this.state.showmenu ? 'block' : ''
+          }}
+        >
+          <li>
+            <a className="btn-white" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="btn-white" href="/events">
+              All events
+            </a>
+          </li>
+          <li>
+            <a className="btn-white" href="/about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="btn-white" href="/videos">
+              Videos
+            </a>
+          </li>
+          <li>
+            <a className="btn-white" href="/contact">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a className="btn-white" href="/speakers">
+              Speakers
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
