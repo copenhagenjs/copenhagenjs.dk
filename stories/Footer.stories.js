35af8caf52f7f6d97cc06eba5d16dcbbf38f42f0
Kevin Simper <kevin.simper@gmail.com>
2019-09-09 12:36:45 +0200

    Add story for footer and react has to be defined

---
+++ stories/Footer.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Footer from '../components/Footer'
+
+storiesOf('Footer', module).add('Default', () => <Footer></Footer>)
