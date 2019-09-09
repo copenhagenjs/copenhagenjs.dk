1310972f91a9d2e9deffffadfc710e655d40f905
Kevin Simper <kevin.simper@gmail.com>
2019-09-09 12:52:13 +0200

    Align prettier to more files

--- stories/TextArea.stories.js
+++ stories/TextArea.stories.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import TextArea from '../components/TextArea';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import TextArea from '../components/TextArea'
 
 storiesOf('Text Area', module).add('Default', () => (
   <TextArea label="Default" name="default" onChange={action('changed')} />
-));
+))
