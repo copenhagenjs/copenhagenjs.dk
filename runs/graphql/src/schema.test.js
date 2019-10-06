ef9392bbae5f61a0e3addc2d2b0942984484a850
Kevin Simper <kevin.simper@gmail.com>
2019-10-07 01:10:24 +0200

    Add test for hello world resolver

--- runs/graphql/src/schema.test.js
+++ runs/graphql/src/schema.test.js
@@ -3,3 +3,7 @@ import { resolvers } from "./schema.js";
 test("resolvers to be defined", () => {
   expect(resolvers).toBeDefined();
 });
+
+test("resolvers to be defined", () => {
+  expect(resolvers.Query.hello()).toBe("Hello world!");
+});
