6bed6dba5a6586d102775d1318b9cb1ae478bb65
Kevin Simper <kevin.simper@gmail.com>
2019-10-08 01:57:54 +0200

    Add test for graphql speakers

---
+++ runs/graphql/src/speakers.test.js
@@ -0,0 +1,31 @@
+const { getEvents } = require("./events.js");
+jest.mock("./events.js");
+const { getSpeakers } = require("./speakers.js");
+
+test("getSpeakers be defined", () => {
+  expect(getSpeakers).toBeDefined();
+});
+
+test("getSpeakers be defined", () => {
+  const events = [
+    {
+      title: "Hello CopenhagenJS",
+      link: "https://copenhagenjs.dk/archive/2011-07-21-hello-copenhagenjs/",
+      markdown: "Welcome",
+      date: new Date(),
+      type: "update",
+      location: "",
+      presentations: [{ name: "Donald Duck", title: "Welcome to Disney" }]
+    }
+  ];
+  getEvents.mockReturnValue(events);
+
+  expect(getSpeakers()).toEqual([
+    {
+      link: "https://copenhagenjs.dk/archive/2011-07-21-hello-copenhagenjs/",
+      name: "Donald Duck",
+      slug: "donald-duck",
+      title: "Welcome to Disney"
+    }
+  ]);
+});
