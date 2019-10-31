f0e2f31892693541fa79faa009fdd231fc706d64
Kevin Simper <kevin.simper@gmail.com>
2019-10-31 16:44:07 +0100

    Change the title to speakers and link to speaker page

--- components/SpeakerSchedule.js
+++ components/SpeakerSchedule.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
+function slugify(string) {
+  const a =
+    'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;'
+  const b =
+    'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+
 export default ({ speakers }) => (
   <div>
-    Schedule:
+    <h2>Speakers:</h2>
     <ul>
-      {speakers.map(s => (
-        <li>
-          {s.title} - {s.name}
+      {speakers.map(speaker => (
+        <li key={speaker}>
+          {speaker.title} -{' '}
+          <strong>
+            <a href={'/speaker?name=' + slugify(speaker.name)}>
+              {speaker.name}
+            </a>
+          </strong>
         </li>
       ))}
     </ul>
