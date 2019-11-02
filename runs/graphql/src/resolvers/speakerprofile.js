56370de41a2a94fcf0cc842f08ef13f7ec9da922
Kevin Simper <kevin.simper@gmail.com>
2019-11-02 21:44:35 +0100

    Lookup a user from a speakerprofile

--- runs/graphql/src/resolvers/speakerprofile.js
+++ runs/graphql/src/resolvers/speakerprofile.js
@@ -1,6 +1,7 @@
 const { memGetEvents } = require("../models/events.js");
 import { getSpeakerProfiles } from "../models/speakers.js";
 import { slugify } from "../services/slug.js";
+import { searchUser } from "../services/firebase.js";
 
 export const speakerProfile = (parent, { slug }) => {
   const parsedPresentations = getSpeakerProfiles().find(
@@ -14,4 +15,10 @@ export const SpeakerPresentationEvent = (parent, args) => {
     event => event.selfLink === parent.selfLink
   );
   return event;
+};
+
+export const SpeakerProfileUser = async (parent, args) => {
+  const users = await searchUser("speakerProfile", parent.slug);
+  if (users.size === 0) return null;
+  return users.docs[0].data();
 };
