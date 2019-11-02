e58217a3ab3907b1bfa7937a988537aed3dadbc9
Kevin Simper <kevin.simper@gmail.com>
2019-11-02 05:56:31 +0100

    Move to new SpeakerProfile type

--- pages/speaker.js
+++ pages/speaker.js
@@ -24,26 +24,14 @@ export const SpeakerProfile = ({ name, presentations = [] }) => (
     <table>
       <thead>
         <tr>
-          <th>Date</th>
           <th>Title</th>
         </tr>
       </thead>
       <tbody>
-        {presentations.reverse().map(speaker => {
-          const date = new Date(parseInt(speaker.event.date))
+        {presentations.reverse().map(title => {
           return (
-            <tr key={speaker.title}>
-              <td>
-                {date
-                  .getDate()
-                  .toString()
-                  .padStart(2, '0')}
-                /{(date.getMonth() + 1).toString().padStart(2, '0')}/
-                {date.getFullYear()}
-              </td>
-              <td>
-                <a href={speaker.event.selfLink}>{speaker.title}</a>
-              </td>
+            <tr key={title}>
+              <td>{title}</td>
             </tr>
           )
         })}
@@ -56,28 +44,23 @@ function Speakers() {
   const slug = getParams().get('name')
   const { loading, error, data } = useQuery(gql`
     {
-      speaker(slug: "${slug}") {
+      speakerProfile(slug: "${slug}") {
         name
-        title
-        event {
-          selfLink
-          date
-        }
+        presentations
       }
     }
   `)
 
   if (loading) return <span>Loading...</span>
   if (error) return <span>Error :(</span>
-  if (data.speaker.length === 0) return <span>Could not find speaker</span>
   return (
     <div>
       <Head>
-        <title>{data.speaker[0].name} spoke at CopenhagenJS</title>
+        <title>{data.speakerProfile.name} spoke at CopenhagenJS</title>
       </Head>
       <SpeakerProfile
-        name={data.speaker[0].name}
-        presentations={data.speaker}
+        name={data.speakerProfile.name}
+        presentations={data.speakerProfile.presentations}
       />
     </div>
   )
