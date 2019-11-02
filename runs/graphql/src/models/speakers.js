aa717b440d8f8168677681608e440a23f0e2e204
Kevin Simper <kevin.simper@gmail.com>
2019-11-02 19:52:17 +0100

    Add test for new SpeakerProfile function

--- runs/graphql/src/models/speakers.js
+++ runs/graphql/src/models/speakers.js
@@ -11,4 +11,18 @@ export const getSpeakers = () => {
     .flat();
 };
 
-export const getSpeakerProfiles = () => {};
+export const getSpeakerProfiles = () => {
+  const events = getEvents();
+  return events
+    .map(event => {
+      const { presentations, selfLink } = event;
+      const profiles = presentations.map(presentation => {
+        return {
+          name: presentation.name,
+          presentations: [{ title: presentation.title, selfLink }]
+        };
+      });
+      return profiles;
+    })
+    .flat();
+};
--- runs/graphql/src/models/speakers.test.js
+++ runs/graphql/src/models/speakers.test.js
@@ -19,7 +19,7 @@ test("getSpeakers be defined", () => {
       presentations: [{ name: "Donald Duck", title: "Welcome to Disney" }]
     }
   ];
-  getEvents.mockReturnValue(events);
+  getEvents.mockReturnValueOnce(events);
 
   expect(getSpeakers()).toEqual([
     {
@@ -34,4 +34,44 @@ test("getSpeakers be defined", () => {
 
 test("getSpeakerProfiles be defined", () => {
   expect(getSpeakerProfiles).toBeDefined();
+});
+
+test("getSpeakerProfiles should return profiles with presentations", () => {
+  const events = [
+    {
+      title: "Hello CopenhagenJS",
+      selfLink:
+        "https://copenhagenjs.dk/archive/2011-07-21-hello-copenhagenjs/",
+      markdown: "Welcome",
+      date: new Date(),
+      type: "update",
+      location: "",
+      presentations: [
+        { name: "Donald Duck", title: "Welcome to Disney" },
+        { name: "Goofy Goof", title: "React.js in Disneyland" }
+      ]
+    }
+  ];
+  getEvents.mockReturnValueOnce(events);
+  const profiles = getSpeakerProfiles();
+  expect(profiles).toEqual([
+    {
+      name: "Donald Duck",
+      presentations: [
+        {
+          title: "Welcome to Disney",
+          selfLink: events[0].selfLink
+        }
+      ]
+    },
+    {
+      name: "Goofy Goof",
+      presentations: [
+        {
+          title: "React.js in Disneyland",
+          selfLink: events[0].selfLink
+        }
+      ]
+    }
+  ]);
 });
