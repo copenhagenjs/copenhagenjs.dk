e9f9fcc0b3519c03fc637a22a7bcbba9f071404a
Kevin Simper <kevin.simper@gmail.com>
2019-11-04 03:34:21 +0100

    Only allow the organizers to get all users

--- runs/graphql/src/resolvers/users.js
+++ runs/graphql/src/resolvers/users.js
@@ -1,1 +1,14 @@
-export const users = () => {};
+import { getUsers } from "../services/firebase.js";
+
+export const users = async (root, args, context) => {
+  if (
+    context.user &&
+    context.user.level &&
+    context.user.level.includes("organizer")
+  ) {
+    const users = await getUsers();
+    return users.map(user => user.data());
+  } else {
+    return [];
+  }
+};
