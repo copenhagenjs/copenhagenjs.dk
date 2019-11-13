22ac45034b16459e02f76a3382f5fcda69bfcd36
Jonas Bak Phillipson <jbakchr@gmail.com>
2019-11-13 19:29:05 +0100

    Created 'laerning-resources.js' and added each learning resource

---
+++ data/learning-resources.js
@@ -0,0 +1,46 @@
+export const resources = [
+  {
+    title: 'freeCodeCamp',
+    desc:
+      'FreeCodeCamp is an interactive online learning platform designed to teach you everything web development related. JavaScript is the most popular language to learn on the platform, and as such, they have multiple tutorials and assignments you can take for free.',
+    url: 'https://www.freecodecamp.org'
+  },
+  {
+    title: 'Codecademy',
+    desc: `Codecademy is like freeCodeCamp, also an interactive platform that offers free coding classes in 12 different programming languages, including Javascript. It's a great starting point if it's your first time learning JS. They do, however, have some paid courses, but most of their programming beginner courses are free.`,
+    url: 'https://www.codecademy.com'
+  },
+  {
+    title: 'Lynda - JavaScript Essential Training',
+    desc:
+      'Lynda is an online platform offering courses in software, creative, and business skills. Their course JavaScript Essential Training offers a good introduction to the language. In addition, there are numerous other courses on JavaScript and related technologies. Lynda is also a subscription-based platform but they offer a one month free trial which is more than enough to cover this course.',
+    url:
+      'https://www.lynda.com/JavaScript-tutorials/JavaScript-Essential-Training/574716-2.html'
+  },
+  {
+    title: 'Treehouse - JavaScript',
+    desc: `Treehouse offers video courses together with interactive exercises to reinforce the concepts covered. They offer a JavaScript Beginner's Track which covers the basics of the language, some ES6, and basics of DOM manipulation. Treehouse is also a subscription-based platform but they offer a 7-day free trial which should be enough to complete the beginner's track.`,
+    url: 'https://teamtreehouse.com/tracks/beginning-javascript'
+  },
+  {
+    title: 'MDN Web Docs',
+    desc:
+      'MDN Web Docs is an accurate and credible platform extensively used by industry practitioners as a reference on topics related to web technologies. Among many other resources, they also offer a learning path for beginners on JavaScript.',
+    url: 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript'
+  },
+  {
+    title: 'Speaking JavaScript',
+    desc: `This book is aimed at programmers who want to learn JavaScript quickly and properly, and JavaScript programmers who want to deepen their skills and/or look up specific topics. Dr. Axel Rauschmayer has written other books on JavaScript and also actively maintains a blog <a href='https://2ality.com'>2ality</a> where he writes mainly about JavaScript. An HTML version of the book is available online for free.`,
+    url: 'http://speakingjs.com'
+  },
+  {
+    title: 'Frontend Masters',
+    desc: `This website has amazing video courses. It is subscription-based. However some previews are available. If you are a student you can get 6 months for free through the <a href="https://frontendmasters.com/welcome/github-student-developers/">GitHub Student Developer Pack.</a>. `,
+    url: 'https://frontendmasters.com/learn/javascript/'
+  },
+  {
+    title: `Kyle Simpson - You Don't Know JS`,
+    desc: `This is a series of books diving deep into the core mechanisms of the JavaScript language.`,
+    url: 'https://github.com/getify/You-Dont-Know-JS'
+  }
+]
