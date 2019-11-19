2bd088fad8b9b1dd9d8812e085ff7b1d57e165e2
Kevin Simper <kevin.simper@gmail.com>
2019-11-19 15:57:51 +0100

    Expand test of slugify to know more cases

--- runs/graphql/src/services/slug.test.js
+++ runs/graphql/src/services/slug.test.js
@@ -7,3 +7,12 @@ test("slugify be defined", () => {
 test("slugify name", () => {
   expect(slugify("Kevin Simper")).toBe("kevin-simper");
 });
+
+test("slugify emoji", () => {
+  expect(slugify("Kevin Simper ✅")).toBe("kevin-simper");
+  expect(slugify("✅")).toBe("");
+});
+
+test("slugify name twice result in the same slug", () => {
+  expect(slugify(slugify("Kevin Simper"))).toBe("kevin-simper");
+});
