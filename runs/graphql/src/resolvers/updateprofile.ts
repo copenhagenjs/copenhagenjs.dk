153ea48deb04bd5e4186b834eff6fdff756da6a1
Kevin Simper <kevin.simper@gmail.com>
2019-11-19 15:56:54 +0100

    Check it after it has been slugified

--- runs/graphql/src/resolvers/updateprofile.ts
+++ runs/graphql/src/resolvers/updateprofile.ts
@@ -1,7 +1,8 @@
 import { updateUser, searchUser } from "../models/user";
 import { slugify } from "../services/slug";
+import { Context } from "../services/context";
 
-export const updateProfile = async (parent, arg, context) => {
+export const updateProfile = async (parent, arg, context: Context) => {
   if (!context.token) {
     throw new Error("Can't update with no user");
   }
@@ -11,12 +12,12 @@ export const updateProfile = async (parent, arg, context) => {
     if (foundUsername.size > 0) {
       throw new Error("Username already exists!");
     }
+    userInput.username = slugify(userInput.username);
     if (userInput.username.length < 8) {
       throw new Error(
         "Username already exists, it has to be min. 8 characters!"
       );
     }
-    userInput.username = slugify(userInput.username);
   }
   const data = await updateUser(context.token.user_id, userInput);
   return arg.input;
