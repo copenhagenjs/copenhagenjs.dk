e195e537edacc711c767502790a55936aa02fa46
Kevin Simper <kevin.simper@gmail.com>
2019-12-01 22:41:34 +0000

    Add new videos

--- data/videos.js
+++ data/videos.js
@@ -1,80 +1,87 @@
 export const videos = [
   [
-    'Zu8P6xejHuU',
-    'Shape your workflows with custom eslint plugins',
-    'Peter Müller'
+    "Zu8P6xejHuU",
+    "Shape your workflows with custom eslint plugins",
+    "Peter Müller"
   ],
   [
-    'HYqxc_zjqMg',
-    'Micro:bit - JavaScript meets microcontroller',
-    'Kenneth Geisshirt'
+    "HYqxc_zjqMg",
+    "Micro:bit - JavaScript meets microcontroller",
+    "Kenneth Geisshirt"
   ],
-  ['VNoDZihhWHI', 'GraphQL and React Suspense', 'Morten Barklund'],
+  ["VNoDZihhWHI", "GraphQL and React Suspense", "Morten Barklund"],
   [
-    '2BYKskAt4rY',
-    'Automatically calculating your own CO2 footprint',
-    'Olivier Corradi'
+    "2BYKskAt4rY",
+    "Automatically calculating your own CO2 footprint",
+    "Olivier Corradi"
   ],
-  ['GUkgcHuhh5k', 'Intelligent use of Electricity', 'Christophe Lephilibert'],
+  ["GUkgcHuhh5k", "Intelligent use of Electricity", "Christophe Lephilibert"],
   [
-    '3plpHThSyXU',
-    'How to support Web Components across browsers with 1 line of code',
-    'Frederik Wessberg'
-  ],
-  [
-    'z7utiFdXgFg',
-    'Building framework agnostic UI components',
-    'Andreas Mehlsen'
+    "3plpHThSyXU",
+    "How to support Web Components across browsers with 1 line of code",
+    "Frederik Wessberg"
   ],
   [
-    'iFKkz6_6WV8',
-    'How we maintain more than 250 Web Components',
-    'Rune Mehlsen'
-  ],
-  ['tty9txryOgA', 'Power of Hackathons', 'André Kovac & Cem Turan'],
-  [
-    'keOPXD-ojWY',
-    'What can a javascript developer do to combat climate change?',
-    'Olivier Corradi'
-  ],
-  ['fO7evPNRZy4', 'A journey == A developer', 'Zoey Zou'],
-  [
-    'IoB6lBWdiew',
-    'Deciding on a frontend framework and how we chose vue.js',
-    'Sumit Kumar'
-  ],
-  ['fEqtq8z2GHk', 'Mythbusting around GraphQL', 'Uri Goldshtein'],
-  ['yAntIlemF28', 'Automated dependency updates', 'Jamie Magee'],
-  [
-    '_WKukhiZhxs',
-    'Being a self-taught javascript developer and what have I learned',
-    'Abdulahi Mohamed'
+    "z7utiFdXgFg",
+    "Building framework agnostic UI components",
+    "Andreas Mehlsen"
   ],
   [
-    'VP8Sa7z7R5Y',
+    "iFKkz6_6WV8",
+    "How we maintain more than 250 Web Components",
+    "Rune Mehlsen"
+  ],
+  ["tty9txryOgA", "Power of Hackathons", "André Kovac & Cem Turan"],
+  [
+    "keOPXD-ojWY",
+    "What can a javascript developer do to combat climate change?",
+    "Olivier Corradi"
+  ],
+  ["fO7evPNRZy4", "A journey == A developer", "Zoey Zou"],
+  [
+    "IoB6lBWdiew",
+    "Deciding on a frontend framework and how we chose vue.js",
+    "Sumit Kumar"
+  ],
+  ["fEqtq8z2GHk", "Mythbusting around GraphQL", "Uri Goldshtein"],
+  ["yAntIlemF28", "Automated dependency updates", "Jamie Magee"],
+  [
+    "_WKukhiZhxs",
+    "Being a self-taught javascript developer and what have I learned",
+    "Abdulahi Mohamed"
+  ],
+  [
+    "VP8Sa7z7R5Y",
     "Probability in TensorFlow.js: Chapter 1 - A programmer's nightmare",
-    'Andreas Madsen'
+    "Andreas Madsen"
   ],
-  ['bYn_f05xfIU', 'Why is my build times so long', 'Kevin Simper'],
-  ['OGMcBOcgEOQ', 'Generators and Async Await', 'Daniel Pouzemski'],
-  ['T998Ezr5xjs', 'Keep Calm And Curry On', 'Andreas Møller'],
-  ['1IFVxE5Z02E', 'One architecture - Four platforms', 'Søren Vind'],
+  ["bYn_f05xfIU", "Why is my build times so long", "Kevin Simper"],
+  ["OGMcBOcgEOQ", "Generators and Async Await", "Daniel Pouzemski"],
+  ["T998Ezr5xjs", "Keep Calm And Curry On", "Andreas Møller"],
+  ["1IFVxE5Z02E", "One architecture - Four platforms", "Søren Vind"],
   [
-    'YSHlwc_euyg',
-    'From JavaScript to TypeScript: how a different approach can make it more helpful',
-    'Eduardo Sorribas'
+    "YSHlwc_euyg",
+    "From JavaScript to TypeScript: how a different approach can make it more helpful",
+    "Eduardo Sorribas"
   ],
-  ['sRBUkS6gT8k', 'HackYourFuture - 2 years later', 'Christopher Klüter'],
+  ["sRBUkS6gT8k", "HackYourFuture - 2 years later", "Christopher Klüter"],
   [
-    'UhGBDtt2E8g',
-    'How we replaced our Drupal CMS with 589 lines of JavaScript',
-    'Allan Ebdrup'
+    "UhGBDtt2E8g",
+    "How we replaced our Drupal CMS with 589 lines of JavaScript",
+    "Allan Ebdrup"
   ],
   [
-    '1YXzTiER5qM',
-    'WebRTC streaming to large audiences',
-    'Vasil Dimitrov & Daniel Pouzemski'
+    "1YXzTiER5qM",
+    "WebRTC streaming to large audiences",
+    "Vasil Dimitrov & Daniel Pouzemski"
   ],
-  ['qkYwXSd-oK4', 'My Coffee App with React Native', 'Martin Lichstam'],
-  ['fqIdpPfnAW8', 'Welcome to CopenhagenJS', 'Zoey Zou']
-]
+  ["qkYwXSd-oK4", "My Coffee App with React Native", "Martin Lichstam"],
+  ["fqIdpPfnAW8", "Welcome to CopenhagenJS", "Zoey Zou"],
+  ["fv1eLnTJLU0", "Typescript - Mapped & Conditional Types", "Pavel Kučera"],
+  [
+    "iA-4xuL_dxE",
+    "Proxy - how to make users of your library love you",
+    "Radoslaw Medryk"
+  ],
+  ["myQ9vqG2qGA", "Using JavaScript to buy a house", "Benjamin Hughes"]
+];
