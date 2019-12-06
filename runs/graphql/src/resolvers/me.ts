4c212beced6ae87292fdce499d7a79a2c7331d68
Kevin Simper <kevin.simper@gmail.com>
2019-12-06 02:02:08 +0100

    Always return the email

--- runs/graphql/src/resolvers/me.ts
+++ runs/graphql/src/resolvers/me.ts
@@ -2,9 +2,6 @@ import { getUser, getUserEmail, User } from "../models/user";
 
 export const me = async (parent, args, context): Promise<User> => {
   const doc = await getUser(context.token.user_id);
-  if (!doc.exists) {
-    throw Error("No such user!");
-  }
   const data = doc.data();
   const email = await getUserEmail(doc.id);
   return {
