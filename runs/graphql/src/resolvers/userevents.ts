b4ad9c735e5b77f7a0d652473a6e5fadc2dd96f7
Kevin Simper <kevin.simper@gmail.com>
2019-12-07 02:45:20 +0100

    Implement unique attendance events

--- runs/graphql/src/resolvers/userevents.ts
+++ runs/graphql/src/resolvers/userevents.ts
@@ -1,5 +1,16 @@
-import { getUserAttendanceRaw } from "../models/attendance";
+import { getUserAttendanceRaw, Attendance } from "../models/attendance";
 import { memGetSingleEvent } from "../models/events";
+
+export const uniqueEventsFromAttendance = (
+  attendances: Attendance[]
+): Attendance[] => {
+  const lastUnique = new Map();
+  attendances.forEach(attendance => {
+    lastUnique.set(attendance.eventSlug, attendance);
+  });
+  const events = Array.from(lastUnique.values());
+  return events;
+};
 
 export const UserEvents = async (parent, args, context) => {
   const attendances = await getUserAttendanceRaw(context.token.user_id);
