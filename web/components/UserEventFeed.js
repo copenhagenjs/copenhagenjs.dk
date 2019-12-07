d8868021dd446b040b766e52d678c15a1d919dc7
Kevin Simper <kevin.simper@gmail.com>
2019-12-07 03:42:21 +0100

    Show an array of events

--- web/components/UserEventFeed.js
+++ web/components/UserEventFeed.js
@@ -1,3 +1,20 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
 
-export default () => <div>Events</div>
+export const UserEventFeed = {
+  tag: ({ events }) => (
+    <div>
+      <h2>Events</h2>
+      <div>
+        {events.map(event => (
+          <div>{event.title}</div>
+        ))}
+      </div>
+    </div>
+  ),
+  fragment: gql`
+    fragment UserEventFeed on Event {
+      title
+    }
+  `
+}
--- web/stories/UserEventFeed.stories.js
+++ web/stories/UserEventFeed.stories.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import UserEventFeed from '../components/UserEventFeed'
+import { UserEventFeed } from '../components/UserEventFeed'
 
-storiesOf('UserEventFeed', module).add('Default', () => <UserEventFeed />)
+storiesOf('UserEventFeed', module).add('Default', () => (
+  <UserEventFeed.tag events={[{ title: 'First' }]} />
+))
