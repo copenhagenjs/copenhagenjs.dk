7a7ef1f9bb530db9a5562a92eed57e1d1db10961
Kevin Simper <kevin.simper@gmail.com>
2019-12-22 00:02:56 +0100

    Align icons and simplify tags

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -105,19 +105,30 @@ export default () => (
           margin: 40px 20px;
         }
       }
+      .social-link {
+        display: block;
+        margin: 2px 0;
+      }
+      .social-icon {
+      }
+      .social-icon-container {
+        display: inline-block;
+        width: 40px;
+        text-align: center;
+        vertical-align: middle;
+      }
       .discord-logo {
         width: 30px;
         height: 30px;
-        vertical-align: middle;
       }
       .github-logo {
         width: 25px;
         height: 25px;
-        vertical-align: middle;
-        margin-right: 6px;
       }
     `}</style>
-    <NewsletterForm />
+    <div>
+      <NewsletterForm />
+    </div>
     <div className="social">
       <h3>Dont miss important news!</h3>
       <div>
@@ -135,54 +146,58 @@ export default () => (
         href="https://www.youtube.com/channel/UCOD8lwED5PAcgmhwymQJsng"
         target="_blank"
         aria-label="Watch our videos"
+        className="social-link"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="50"
-          viewBox="0 0 256 180"
-          style={{ verticalAlign: 'bottom' }}
-        >
-          <defs>
-            <clipPath id="a">
-              <path d="M0 192h192V0H0z" />
-            </clipPath>
-          </defs>
-          <g clipPath="url(#a)" transform="matrix(1,0,0,1,34,0)">
-            <path
-              d="M180.322 138.637c-2.024 7.622-7.987 13.624-15.56 15.661C151.036 158 96 158 96 158s-55.037 0-68.762-3.702c-7.573-2.037-13.537-8.039-15.56-15.66C8 124.822 8 96 8 96s0-28.822 3.677-42.638c2.024-7.62 7.988-13.623 15.561-15.66C40.963 34 96 34 96 34s55.037 0 68.761 3.701c7.574 2.038 13.537 8.04 15.561 15.661C184 67.178 184 96 184 96s0 28.823-3.678 42.637"
-              fill="red"
-            />
-            <path d="M78 69.831L124 96l-46 26.17z" fill="#fff" />
-          </g>
-        </svg>
+        <div className="social-icon-container">
+          <svg
+            className="social-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            viewBox="0 0 256 180"
+          >
+            <defs>
+              <clipPath id="a">
+                <path d="M0 192h192V0H0z" />
+              </clipPath>
+            </defs>
+            <g clipPath="url(#a)" transform="matrix(1,0,0,1,34,0)">
+              <path
+                d="M180.322 138.637c-2.024 7.622-7.987 13.624-15.56 15.661C151.036 158 96 158 96 158s-55.037 0-68.762-3.702c-7.573-2.037-13.537-8.039-15.56-15.66C8 124.822 8 96 8 96s0-28.822 3.677-42.638c2.024-7.62 7.988-13.623 15.561-15.66C40.963 34 96 34 96 34s55.037 0 68.761 3.701c7.574 2.038 13.537 8.04 15.561 15.661C184 67.178 184 96 184 96s0 28.823-3.678 42.637"
+                fill="red"
+              />
+              <path d="M78 69.831L124 96l-46 26.17z" fill="#fff" />
+            </g>
+          </svg>
+        </div>
         Subscribe on Youtube
       </a>
-      <div>
-        <a
-          href="https://github.com/copenhagenjs/copenhagenjs.dk"
-          target="_blank"
-        >
+      <a
+        href="https://github.com/copenhagenjs/copenhagenjs.dk"
+        target="_blank"
+        className="social-link"
+      >
+        <div className="social-icon-container">
           <svg
-            className="github-logo"
+            className="github-logo social-icon"
             role="img"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
           </svg>
-          Contribute on GitHub
-        </a>
-      </div>
-      <div className="discord">
-        <a href="https://discord.gg/7vxce3s">
+        </div>
+        Contribute on GitHub
+      </a>
+      <a href="https://discord.gg/7vxce3s" className="social-link">
+        <div className="social-icon-container">
           <img
-            className="discord-logo"
+            className="discord-logo social-icon"
             src="/static/images/discord-logo.svg"
             alt="copenhagenjs discord"
           />
-          Join our Discord
-        </a>
-      </div>
+        </div>
+        Join our Discord
+      </a>
     </div>
 
     <div>
