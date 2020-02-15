10ac38fc59b6b4fe1afa6b2093524a3cddc850e5
Kevin Simper <kevin.simper@gmail.com>
2020-02-15 19:19:31 +0100

    Update model with userid

--- runs/graphql/src/models/presentationdetails.ts
+++ runs/graphql/src/models/presentationdetails.ts
@@ -7,14 +7,16 @@ import {
 import admin from "firebase-admin";
 
 export type PresentationDetails = {
+  timestamp?: string;
+  userId?: string;
   name?: string;
   title?: string;
-  timestamp?: string;
   text?: string;
   link?: string;
 };
 
 export type PresentationDetailsInput = {
+  userId: string;
   name: string;
   title: string;
   text: string;
@@ -36,6 +38,7 @@ export async function addPresentationDetail(
 
 if(require.main === module) {
   addPresentationDetail({
+    userId: '123abc',
     name: 'kevin',
     title: 'tech',
     text: 'github',
