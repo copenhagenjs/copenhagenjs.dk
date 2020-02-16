3f0cb7ae2a083821326aaf8af5d0a13e46e82db9
Kevin Simper <kevin.simper@gmail.com>
2020-02-16 19:02:18 +0100

    Change to event instead of name

--- web/pages/presentation.js
+++ web/pages/presentation.js
@@ -9,15 +9,15 @@ import { getParams } from '../services/url'
 function Presentation() {
   const { loading, error, data } = useQuery(
     gql`
-      query Presentations($nameslug: String!, $titleslug: String!) {
-        presentation(nameslug: $nameslug, titleslug: $titleslug) {
+      query Presentations($eventslug: String!, $titleslug: String!) {
+        presentation(eventslug: $eventslug, titleslug: $titleslug) {
           title
         }
       }
     `,
     {
       variables: {
-        nameslug: getParams().get('name'),
+        eventslug: getParams().get('event'),
         titleslug: getParams().get('title')
       }
     }
@@ -26,20 +26,14 @@ function Presentation() {
   if (loading) return <span>Loading...</span>
   if (error) return <span>Error :(</span>
 
-  return (
-    <div>
-      {data.presentation.title}
-    </div>
-  )
+  return <div>{data.presentation.title}</div>
 }
 
 export default () => (
   <ApolloProvider client={client}>
     <Page>
-      <h1>
-        Presentations
-      </h1>
-      <Presentation/>
+      <h1>Presentation</h1>
+      <Presentation />
     </Page>
   </ApolloProvider>
 )
