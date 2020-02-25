b60d01c926360bd37e472bb037b734b4e4c126af
Kevin Simper <kevin.simper@gmail.com>
2020-02-25 17:13:59 +0100

    Implement logic to get presentation details

--- runs/graphql/src/resolvers/speakerpresentation.test.ts
+++ runs/graphql/src/resolvers/speakerpresentation.test.ts
@@ -1,5 +1,39 @@
-import { SpeakerPresentationDetails } from "./speakerpresentation"
+import { SpeakerPresentationDetails } from "./speakerpresentation";
+
+jest.mock("../models/events");
+import { memGetEvents } from "../models/events";
+const mockedMemGetEvents = memGetEvents as jest.MockedFunction<
+  typeof memGetEvents
+>;
+
+jest.mock("../models/presentationdetails");
+import { getPresentationDetails } from "../models/presentationdetails";
+const mockedGetPresentationDetails = getPresentationDetails as jest.MockedFunction<
+  typeof getPresentationDetails
+>;
 
 test("SpeakerPresentationDetail defined", () => {
   expect(SpeakerPresentationDetails).toBeDefined();
+});
+
+test("SpeakerPresentationDetails should return an array of details", async () => {
+  const selfLink = "example";
+  const slug = "slug";
+  const title = "test";
+  mockedMemGetEvents.mockReturnValueOnce([
+    {
+      selfLink,
+      slug,
+      date: new Date(),
+      presentations: []
+    }
+  ]);
+  mockedGetPresentationDetails.mockReturnValueOnce([]);
+  const SpeakerPresentation = {
+    title,
+    selfLink
+  };
+  const details = await SpeakerPresentationDetails(SpeakerPresentation);
+  expect(details).toEqual([]);
+  expect(mockedGetPresentationDetails).toBeCalledWith(slug, title);
 });
--- runs/graphql/src/resolvers/speakerpresentation.ts
+++ runs/graphql/src/resolvers/speakerpresentation.ts
@@ -1,3 +1,15 @@
-export const SpeakerPresentationDetails = (parent) => {
+import { SpeakerProfilePresentation } from "../models/speakers";
+import { getPresentationDetails } from "../models/presentationdetails";
+import { memGetEvents } from "../models/events";
 
-}
+export const SpeakerPresentationDetails = async (
+  parent: SpeakerProfilePresentation
+) => {
+  const event = memGetEvents().find(
+    event => event.selfLink === parent.selfLink
+  );
+  if (!event) {
+    throw new Error("Did not find event!");
+  }
+  return getPresentationDetails(event.slug, parent.title);
+};
