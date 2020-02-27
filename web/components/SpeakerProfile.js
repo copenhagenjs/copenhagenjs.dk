5b7153f4b0cf1a060cf7e3c0025750b9f4e4c5b5
Kevin Simper <kevin.simper@gmail.com>
2020-02-27 18:34:13 +0100

    Refactor out speakerprofilevideos

--- web/components/SpeakerProfile.js
+++ web/components/SpeakerProfile.js
@@ -1,44 +1,6 @@
 import React from 'react'
 import { gql } from 'apollo-boost'
-import { Embed } from './YoutubeEmbed.js'
-
-export const SpeakerProfileVideos = {
-  tag: ({ videos }) => {
-    if (videos.length === 0) return null
-    return (
-      <div style={{ margin: '0 0 20px' }}>
-        <style jsx>{`
-          .video {
-            margin-bottom: 20px;
-          }
-          @media (min-width: 700px) {
-            .videos {
-              display: flex;
-            }
-            .video {
-              width: 50%;
-              margin: 0 20px 0 0;
-            }
-          }
-        `}</style>
-        Videos:
-        <div className="videos">
-          {videos.map(video => (
-            <div className="video" key={video}>
-              <Embed youtubeId={video.youtubeId} />
-            </div>
-          ))}
-        </div>
-      </div>
-    )
-  },
-  fragment: gql`
-    fragment SpeakerProfileVideos on Video {
-      title
-      youtubeId
-    }
-  `
-}
+import { SpeakerProfileVideos } from "./SpeakerProfileVideos";
 
 export const SpeakerProfile = ({
   name,
---
+++ web/components/SpeakerProfileVideos.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Embed } from './YoutubeEmbed.js'
+
+export const SpeakerProfileVideos = {
+  tag: ({ videos }) => {
+    if (videos.length === 0) return null
+    return (
+      <div style={{ margin: '0 0 20px' }}>
+        <style jsx>{`
+          .video {
+            margin-bottom: 20px;
+          }
+          @media (min-width: 700px) {
+            .videos {
+              display: flex;
+            }
+            .video {
+              width: 50%;
+              margin: 0 20px 0 0;
+            }
+          }
+        `}</style>
+        Videos:
+        <div className="videos">
+          {videos.map(video => (
+            <div className="video" key={video}>
+              <Embed youtubeId={video.youtubeId} />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  },
+  fragment: gql`
+    fragment SpeakerProfileVideos on Video {
+      title
+      youtubeId
+    }
+  `
+}
